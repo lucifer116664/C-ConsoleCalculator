10ed7522cf196df72b034a356c04943058511686
lucifer116664 <102432996+lucifer116664@users.noreply.github.com>
2022-06-11 17:44:49 +0300

    Add files via upload

---
+++ IfElseCalc/IfElse.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace SwitchCalc
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+        error: //label for goto
+            Console.WriteLine("Введіть операнди:");
+            float a, b;
+            bool isWight = float.TryParse(Console.ReadLine(), out a);
+            if (!isWight)
+            {
+                Console.WriteLine("Ви ввели неправильні дані. Спробуйте ще раз:");
+                goto error;
+            }
+            isWight = float.TryParse(Console.ReadLine(), out b);
+            if (!isWight)
+            {
+                Console.WriteLine("Ви ввели неправильні дані. Спробуйте ще раз:");
+                goto error;
+            }
+            Console.WriteLine("Яку дію Ви хочете виконати?");
+            string choose = Console.ReadLine();
+
+            if(choose == "+")
+            {
+                Console.WriteLine(a + " + " + b + " = " + (a + b));
+            }
+            else
+            {
+                if (choose == "-")
+                {
+                    Console.WriteLine(a + " - " + b + " = " + (a - b));
+                }
+                else
+                {
+                    if (choose == "*")
+                    {
+                        Console.WriteLine(a + " * " + b + " = " + (a * b));
+                    }
+                    else
+                    {
+                        if (choose == "/")
+                        {
+                            Console.WriteLine(a + " / " + b + " = " + (a / b));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Такої дії не існує.");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
